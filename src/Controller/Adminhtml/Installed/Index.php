c664e6c989061909f09332456d3b4aabb4cd6f9a
Lau Bannenberg <lau.bannenberg@gmail.com>
2025-12-25 01:03:47 +0100

    wire up installed packages index (dummy contents for now)

---
+++ src/Controller/Adminhtml/Installed/Index.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+namespace Corrivate\ComposerDashboard\Controller\Adminhtml\Installed;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index implements HttpGetActionInterface
+{
+    public const ADMIN_RESOURCE = 'Corrivate_ComposerDashboard::composerdashboard';
+
+    public function __construct(private readonly PageFactory $resultPageFactory)
+    {
+    }
+
+    public function execute(): ResultInterface
+    {
+        $title = 'Composer > Installed Packages';
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('Magento_Backend::system');
+        $resultPage->addBreadcrumb(__($title), __($title));
+        $resultPage->getConfig()->getTitle()->prepend(__($title));
+        return $resultPage;
+    }
+}
+
